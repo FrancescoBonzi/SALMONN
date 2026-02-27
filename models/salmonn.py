1a2821118fcf1fad2d3d0cae480f7526c30028f8
Francesco Bonzi <fra.bonzi@gmail.com>
2026-02-27 11:30:02 -0500

    Change alpha range for Conclusion Focus SALMONN

--- models/salmonn.py
+++ models/salmonn.py
@@ -662,8 +662,9 @@ class ConclusionFocusSALMONN(SALMONN):
         "<REASONING>", "</REASONING>",
         "<CONCLUSION>", "</CONCLUSION>",
     ]
-    def __init__(self, alpha_max=5.0, *args, **kwargs):
+    def __init__(self, alpha_min=0.1, alpha_max=2.0, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.alpha_min = alpha_min
         self.alpha_max = alpha_max
         self.llama_tokenizer.add_special_tokens({"additional_special_tokens": self.CHAPTER_SPECIAL_TOKENS})
         self.llama_model.resize_token_embeddings(len(self.llama_tokenizer))
@@ -757,10 +758,80 @@ class ConclusionFocusSALMONN(SALMONN):
             loss_conclusion = F.cross_entropy(logits[conclusion_mask, :], targets[conclusion_mask], ignore_index=-100)
             loss_other_chapters = F.cross_entropy(logits[~conclusion_mask, :], targets[~conclusion_mask], ignore_index=-100)
 
-            alpha = random.uniform(1.0, self.alpha_max)
+            alpha = random.uniform(self.alpha_min, self.alpha_max)
             loss = alpha * loss_conclusion + loss_other_chapters
 
         return {"loss": loss}
+
+    @classmethod
+    def from_config(cls, config):
+        alpha_min = config.get("alpha_min", 0.1)
+        alpha_max = config.get("alpha_max", 2.0)
+        llama_path = config.get("llama_path")
+        whisper_path = config.get("whisper_path")
+        freeze_whisper = config.get("freeze_whisper", True)
+        beats_path = config.get("beats_path", "")
+        freeze_beats = config.get("freeze_beats", True)
+
+        use_speech_Qformer = config.get("use_speech_Qformer", True)
+        num_speech_query_token = config.get("num_speech_query_token", 1)
+        freeze_speech_QFormer = config.get("freeze_speech_QFormer", False)
+        window_level_Qformer = config.get("window_level_Qformer", True)
+        second_per_window = config.get("second_per_window", 0.333333)
+        second_stride = config.get("second_stride", 0.333333)
+
+        speech_llama_proj_model = config.get("speech_llama_proj_model", "")
+        freeze_speech_llama_proj = config.get("freeze_speech_llama_proj", False)
+
+        lora = config.get("lora", True)
+        lora_rank = config.get("lora_rank", 8)
+        lora_alpha = config.get("lora_alpha", 32)
+        lora_dropout = config.get("lora_dropout", 0.1)
+
+        multi_prompt = config.get("multi_prompt", False)
+        prompt_path = config.get("prompt_path", "")
+        prompt_template = config.get("prompt_template", "")
+        max_txt_len = config.get("max_txt_len", 128)
+        end_sym = config.get("end_sym", "</s>")
+        low_resource = config.get("low_resource", False)
+        device_8bit = config.get("device_8bit", 0)
+
+        model = cls(
+            alpha_min=alpha_min,
+            alpha_max=alpha_max,
+            llama_path=llama_path,
+            whisper_path=whisper_path,
+            freeze_whisper=freeze_whisper,
+            beats_path=beats_path,
+            freeze_beats=freeze_beats,
+            use_speech_Qformer=use_speech_Qformer,
+            num_speech_query_token=num_speech_query_token,
+            freeze_speech_QFormer=freeze_speech_QFormer,
+            window_level_Qformer=window_level_Qformer,
+            second_per_window=second_per_window,
+            second_stride=second_stride,
+            speech_llama_proj_model=speech_llama_proj_model,
+            freeze_speech_llama_proj=freeze_speech_llama_proj,
+            lora=lora,
+            lora_rank=lora_rank,
+            lora_alpha=lora_alpha,
+            lora_dropout=lora_dropout,
+            multi_prompt=multi_prompt,
+            prompt_path=prompt_path,
+            prompt_template=prompt_template,
+            max_txt_len=max_txt_len,
+            end_sym=end_sym,
+            low_resource=low_resource,
+            device_8bit=device_8bit,
+        )
+
+        ckpt_path = config.get("ckpt", "")
+        if ckpt_path:
+            logging.info("Load ConclusionFocusSALMONN ckpt from: {}".format(ckpt_path))
+            ckpt = torch.load(ckpt_path, map_location="cpu")
+            model.load_state_dict(ckpt['model'], strict=False)
+
+        return model
 
 
 class ReverseSALMONN(SALMONN):
